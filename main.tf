0422bc55b70af45f68e536b27c2111605fec1aae
Andrei Karshkov <karshkoff@gmail.com>
2025-10-09 00:21:47 +0200

    k8s admin access

--- main.tf
+++ main.tf
@@ -2,14 +2,6 @@ provider "aws" {
   region = var.region
 }
 
-# Backend S3 bucket for Terraform state
-
-module "storage" {
-  source         = "./modules/storage"
-  backend_bucket = "${var.tags.project}-tf-state-bucket"
-  tags           = var.tags
-}
-
 # VPC for the infrastructure
 
 module "network" {
--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -1,13 +1,3 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.main.id
-}
-
-output "public_subnet_ids" {
-  description = "The IDs of the public subnets"
-  value       = [aws_subnet.public_01.id, aws_subnet.public_02.id]
-}
-
 output "private_subnet_ids" {
   description = "The IDs of the private subnets"
   value       = [aws_subnet.private_01.id, aws_subnet.private_02.id]
