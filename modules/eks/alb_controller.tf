c2a749dbdfda4e97a28d5c96bc480eee13e2f350
Andrei Karshkov <karshkoff@gmail.com>
2025-10-12 16:14:05 +0200

    LBC with AWS ALB

--- modules/eks/alb_controller.tf
+++ modules/eks/alb_controller.tf
@@ -1,39 +1,32 @@
-provider "kubernetes" {
-  host                   = local.host
-  cluster_ca_certificate = local.cluster_ca_certificate
-  token                  = local.token
-}
+# provider "kubernetes" {
+#   host                   = local.host
+#   cluster_ca_certificate = local.cluster_ca_certificate
+#   token                  = local.token
+# }
 
 provider "helm" {
   kubernetes = {
-    host                   = local.host
-    cluster_ca_certificate = local.cluster_ca_certificate
-    token                  = local.token
+    host                   = aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.main.token
   }
-}
-
-locals {
-  host                   = aws_eks_cluster.main.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.main.token
-
 }
 
 data "aws_eks_cluster_auth" "main" {
   name = var.cluster_name
 }
 
-# ALB controller service account
+# # ALB controller service account
 
-resource "kubernetes_service_account" "alb" {
-  metadata {
-    name      = "aws-load-balancer-controller"
-    namespace = "kube-system"
-    annotations = {
-      "eks.amazonaws.com/role-arn" = "arn:aws:iam::${var.aws_account_id}:role/${var.alb_controller_role_name}"
-    }
-  }
-}
+# resource "kubernetes_service_account" "alb" {
+#   metadata {
+#     name      = "aws-load-balancer-controller"
+#     namespace = "kube-system"
+#     annotations = {
+#       "eks.amazonaws.com/role-arn" = "arn:aws:iam::${var.aws_account_id}:role/${var.alb_controller_role_name}"
+#     }
+#   }
+# }
 
 # ALB controller
 
@@ -52,16 +45,16 @@ resource "helm_release" "aws_load_balancer_controller" {
       value = var.cluster_name
     },
     {
-      name  = "serviceAccount.create"
-      value = "false"
-    },
-    {
       name  = "serviceAccount.name"
       value = var.alb_controller_role_name
     },
     {
       name  = "vpcId"
       value = var.vpc_id
+    },
+    {
+      name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+      value = "arn:aws:iam::${var.aws_account_id}:role/${var.alb_controller_role_name}"
     },
   ]
 }
