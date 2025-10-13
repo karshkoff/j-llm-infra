f0d3a7da6c109c27146e1b54157963e6da9d5097
Andrei Karshkov <karshkoff@gmail.com>
2025-10-13 22:53:22 +0200

    TG and Listeners to LBC

--- modules/network/alb.tf
+++ modules/network/alb.tf
@@ -7,86 +7,17 @@ resource "aws_lb" "main" {
   security_groups    = [aws_security_group.alb_sg.id]
   subnets            = [aws_subnet.public_01.id, aws_subnet.public_02.id]
 
-  tags = merge(var.tags, { Name = "${var.tags.project}-alb" })
+  tags = merge(var.tags,
+    {
+      Name                       = "${var.tags.project}-alb",
+      "elbv2.k8s.aws/cluster"    = var.tags.project,
+      "ingress.k8s.aws/stack"    = "${var.tags.project}/${var.tags.project}-ingress",
+      "ingress.k8s.aws/resource" = "LoadBalancer",
+    }
+  )
 }
 
-resource "aws_lb_target_group" "main" {
-  name     = "${var.tags.project}-tg"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200-399"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
-
-  tags = merge(var.tags, { Name = "${var.tags.project}-tg" })
-}
-
-data "aws_acm_certificate" "cert" {
-  domain   = "*.${var.domain_name}"
-  statuses = ["ISSUED"]
-}
-
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = data.aws_acm_certificate.cert.arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.main.arn
-  }
-}
-
-# Security Group for ALB
-
-resource "aws_security_group" "alb_sg" {
-  name        = "${var.tags.project}-alb-sg"
-  description = "ALB security group"
-  vpc_id      = aws_vpc.main.id
-
-  tags = var.tags
-}
-
-resource "aws_vpc_security_group_ingress_rule" "specific_ip_tls_ipv4" {
-  security_group_id = aws_security_group.alb_sg.id
-  from_port         = 443
-  to_port           = 443
-  ip_protocol       = "tcp"
-  cidr_ipv4         = "188.91.152.191/32"
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.alb_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # All protocols
-}
-
-# # Allow traffic from ALB to EKS nodes
-
-# data "aws_security_group" "eks_cluster_sg" {
-#   filter {
-#     name   = "tag:kubernetes.io/cluster/${var.tags.project}-eks-cluster"
-#     values = ["owned"]
-#   }
-# }
-
-# resource "aws_vpc_security_group_ingress_rule" "allow_alb_to_eks_nodes" {
-#   security_group_id            = data.aws_security_group.eks_cluster_sg.id
-#   referenced_security_group_id = aws_security_group.alb_sg.id
-#   ip_protocol                  = "-1" # All protocols
-#   description                  = "Traffic from ALB to EKS nodes"
-# }
-
+# Route53 Record for ALB
 
 data "aws_route53_zone" "main" {
   name         = var.domain_name
